cff4e57f01c379b3a8d3f16d0ff425ef758e0f2c
w50111831 <danielhitchcock236@proton.me>
2026-09-09 21:55:43 +0100

    Add auth dummy routes.

--- backend/server.ts
+++ backend/server.ts
@@ -9,10 +9,47 @@ await app.register(cors, {
   origin: "http://localhost:5173",
 });
 
+//Auth routes
+
+app.post("/api/auth/sign-up", async() => {
+  return {
+    dummy: "sign-up route works",
+  }
+})
+
+app.post("/api/auth/sign-in", async() => {
+  return {
+    dummy: "sign-in route works",
+  }
+})
+
+app.post("/api/auth/sign-out", async() => {
+  return {
+    dummy: "sign-out route works",
+  }
+})
+
 app.get("/api/hello", async() => {
   return {
     status: "works!",
     message: "HELLO WORLD",
+  }
+})
+
+app.get("/api/study-plans", async () => {
+  return {
+    plans: [
+      {
+        id: 1,
+        title: "Mathematics plan",
+        description: "Plan for studying mathematics",
+      },
+      {
+        id: 2,
+        title: "Computer science plan",
+        description: "Do hackathon project",
+      }
+    ]
   }
 })
 
